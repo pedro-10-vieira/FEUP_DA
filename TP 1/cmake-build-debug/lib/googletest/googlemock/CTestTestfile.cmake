c5a726a25595e4e64a4273e9494e8536b36f97bc
Pedro Jorge das Neves Pinto Vieira <up202206230@up.pt>
2024-02-11 19:36:19 +0000

    Unnecessary files removed

--- TP 1/cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Pedro Vieira/Desktop/FEUP/DA/TP 1/lib/googletest/googlemock
-# Build directory: C:/Users/Pedro Vieira/Desktop/FEUP/DA/TP 1/cmake-build-debug/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
