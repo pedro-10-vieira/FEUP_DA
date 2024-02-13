6ebf7f61785ac6bbed6644f9c3a95a03a9a2d15f
Pedro Jorge das Neves Pinto Vieira <up202206230@up.pt>
2024-02-13 14:01:00 +0000

    Exercise 1 done

--- TP 1/TP1/ex1.cpp
+++ TP 1/TP1/ex1.cpp
@@ -12,23 +12,22 @@ template <typename T>
  * Follows the algorithm described in theoretical classes.
  */
 vector<T> topsort(const Graph<T>* g) {
-    Graph<T> copy = *g;
     if (!isDAG(g)) {
         return vector<T>();
     }
     vector<T> res;
     queue<Vertex<T>*> v_queue;
-    for (Vertex<T>* v : copy.getVertexSet()) {
+    for (Vertex<T>* v : g->getVertexSet()) {
         v->setIndegree(0);
     }
-    for (Vertex<T>* v : copy.getVertexSet()) {
+    for (Vertex<T>* v : g->getVertexSet()) {
         for (Edge<T> edge : v->getAdj()) {
             Vertex<T>* e = edge.getDest();
             T value = e->getIndegree();
             e->setIndegree(value + 1);
         }
     }
-    for (Vertex<T>* v : copy.getVertexSet()) {
+    for (Vertex<T>* v : g->getVertexSet()) {
         if (v->getIndegree() == 0) {
             v_queue.push(v);
         }
@@ -38,7 +37,6 @@ vector<T> topsort(const Graph<T>* g) {
         auto adj = u->getAdj();
         for (Edge<T> edge : adj) {
             auto v = edge.getDest();
-//            copy.removeEdge(u->getInfo(), v->getInfo());
             auto value = v->getIndegree();
             v->setIndegree(value - 1);
             if (v->getIndegree() == 0) {
@@ -47,7 +45,6 @@ vector<T> topsort(const Graph<T>* g) {
         }
         v_queue.pop();
         res.push_back(u->getInfo());
-//        copy.removeVertex(u->getInfo());
     }
     return res;
 }
